f7c9d972e86150c90c00d915de0351ef9e8fcf7c
Arnav-bhoria <82876881+Arnav-bhoria@users.noreply.github.com>
2021-06-17 15:11:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+menu_list_array = ["Veg Margherita Pizza","Veg Paneer Pizza","Tandoori Pizza"
+                    ];
+
+function getmenu(){
+var htmldata;
+console.log(menu_list_array)
+document.getElementById("viewblock").innerHTML=menu_list_array;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
